4f79e27bbdee2dbe591e0ceebdfd7f68ac1b8328
Zachary Olpin <info@zacharyolp.in>
2020-02-03 07:29:44 -0700

    make responsive for mobile

--- public/index.php
+++ public/index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Zachary Olpin</title>
-  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+  <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
 </head>
+
 <body>
   <header>
-  <a class="logo" href="/index.php">Zachary Olpin</a>
+    <a class="logo" href="/index.php">Zachary Olpin</a>
     <nav>
-      <ul class="nav-list">
-        <li class="nav-link"><a href="/pages/assignments/a1.php">A1</a></li>
-        <li class="nav-link"><a href="/pages/assignments/a2.php">A2</a></li>
-        <li class="nav-link"><a href="/pages/assignments/a3.php">A3</a></li>
+      <a id="mobileNav">Menu</a>
+      <ul class="nav-list" id="nav-list">
+        <li class="nav-link"><a href="/pages/assignments/assignments.php">Assignments</a></li>
         <li class="nav-link"><a href="/pages/assignments/shop.php">Shop</a></li>
         <li class="nav-link"><a href="/pages/exercises/exercises.php">Exercises</a></li>
       </ul>
@@ -22,28 +23,30 @@
   </header>
   <main>
     <h2>About</h2>
-    <hr>
-    <img src="/assets/imgs/me.jpg" alt="Portrait of the author" width="250px" />
-    <p>
-      Hi! I'm Zachary. I'm a first-semester student in the CSIS program at Salt Lake Community College.
-      I also have a degree in Psychology from the University of Utah.
-      I have taught myself a small bit of programming in my own time
-      (mostly in Python and Javascript) but am otherwise new to world
-      of Computer Science. You can view a few of my personal projects on
-      my <a href="https://github.com/vxxce/">Github</a>.
-    </p>
-    <p>
-      I am also a painter! I'm more of a hobbyist than a professional,
-      but you can see my work on display now at the Bountiful Davis Art Center.
-      The solo show is called <em>Having Been Utterly Persuaded By a Stone</em>
-      and will be up until Feb. 14th.
-    </p>
-    <p>
-      Some of my other hobbies include the board game "Go" (AKA Baduk or Weiqi),
-      photography, camping, bookbinding, and cycling. I could never settle on
-      a favorite film or book but the first two that come to mind are, respectively,
-      <em>Waking Life</em> and <em>Behave: The Biology of Humans at Our Best and Worst</em>.
-    </p>
+    <div class='hr'></div>
+    <div id="about">
+      <section>
+        Hi! I'm Zachary. I'm a first-semester student in the CSIS program at Salt Lake Community College.
+        I also have a degree in Psychology from the University of Utah.
+        I have taught myself a small bit of programming in my own time
+        (mostly in Python and Javascript) but am otherwise new to world
+        of Computer Science. You can view a few of my personal projects on
+        my <a href="https://github.com/vxxce/">Github</a>.
+        <p>
+          I am also a painter! I'm more of a hobbyist than a professional,
+          but you can see my work on display now at the Bountiful Davis Art Center.
+          The solo show is called <em>Having Been Utterly Persuaded By a Stone</em>
+          and will be up until Feb. 14th.
+          <p>
+            Some of my other hobbies include the board game "Go" (AKA Baduk or Weiqi),
+            photography, camping, bookbinding, and cycling. I could never settle on
+            a favorite film or book but the first two that come to mind are, respectively,
+            <em>Waking Life</em> and <em>Behave: The Biology of Humans at Our Best and Worst</em>.
+      </section>
+      <img id="author-img" src="/assets/imgs/me.jpg" alt="Portrait of the author" />
+      <script src="/assets/js/navbar.js" type="application/javascript"></script>
+    </div>
   </main>
 </body>
+
 </html>
