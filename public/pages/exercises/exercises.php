8a149602095a6e64c115c38b681d130101d5563b
Zachary Olpin <info@zacharyolp.in>
2020-02-03 07:25:20 -0700

    make subdirectory page for chapter exercises

--- public/pages/exercises/exercises.php
+++ public/pages/exercises/exercises.php
@@ -4,17 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Zachary Olpin</title>
+  <title>Exercises</title>
   <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
 </head>
 <body>
   <header>
   <a class="logo" href="/index.php">Zachary Olpin</a>
-    <nav>
-      <ul class="nav-list">
-        <li class="nav-link"><a href="/pages/assignments/a1.php">A1</a></li>
-        <li class="nav-link"><a href="/pages/assignments/a2.php">A2</a></li>
-        <li class="nav-link"><a href="/pages/assignments/a3.php">A3</a></li>
+  <nav>
+      <a id="mobileNav">Menu</a>
+      <ul class="nav-list" id="nav-list">
+        <li class="nav-link"><a href="/pages/assignments/assignments.php">Assignments</a></li>
         <li class="nav-link"><a href="/pages/assignments/shop.php">Shop</a></li>
         <li class="nav-link"><a href="/pages/exercises/exercises.php">Exercises</a></li>
       </ul>
@@ -22,6 +21,24 @@
   </header>
   <main>
     <h2>Exercises</h2>
+    <div class='hr'></div>
+    <div class="directory">
+      <div class="dirButton"><a href="#">Hello World</a></div>
+      <div class="dirButton"><a href="/pages/exercises/ce02/randomEvents.php">Random Events</a></div>
+      <div class="dirButton"><a href="/pages/exercises/ce03/FormPage.php">Using Forms</a></div>
+      <div class="dirButton"><a href="#">Reading Files</a></div>
+      <div class="dirButton"><a href="#">Workbench Setup</a></div>
+      <div class="dirButton"><a href="#">Database CRUD</a></div>
+      <div class="dirButton"><a href="#">Session & Login</a></div>
+      <div class="dirButton"><a href="#">JS Page</a></div>
+      <div class="dirButton"><a href="#">JS Control</a></div>
+      <div class="dirButton"><a href="#">JS Objects</a></div>
+      <div class="dirButton"><a href="#">Validation</a></div>
+      <div class="dirButton"><a href="#">AJAX</a></div>
+      <div class="dirButton"><a href="#">Shopping Cart</a></div>
+    </div>
   </main>
+  <script src="/assets/js/subDir.js" type="application/javascript"></script>
+  <script src="/assets/js/navbar.js" type="application/javascript"></script>
 </body>
 </html>
