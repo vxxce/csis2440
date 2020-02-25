58757cac45669966a379d4cdfb9d5bf5839f8edc
Zachary Olpin <info@zacharyolp.in>
2020-02-24 22:20:41 -0700

    ce05 page with screenshot of sequel pro

---
+++ public/pages/exercises/ce05/screenshot.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CE05</title>
+  <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+</head>
+<body>
+  <?php require("pages/headerNav.php"); ?>
+  <figure>
+  <img src="/assets/imgs/mysqlAdminGui.png" width="75%" alt="Screenshot of Sequel Pro" />
+  <figcaption>Screenshot of Sequel Pro</figcaption>
+  </figure>
+</body>
+
+</html>
