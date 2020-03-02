fceb83775938de24ef75863e3e77642253b4c526
Zachary Olpin <info@zacharyolp.in>
2020-03-02 01:28:49 -0700

    create table syntax for ce07

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- create planets table
+-- create planets table for ce06 in database slcc
 
 CREATE TABLE `planets` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -12,3 +12,16 @@ CREATE TABLE `planets` (
 -- reset auto_increment on planets
 
 ALTER TABLE `planets` AUTO_INCREMENT=1
+
+-- create planets table for ce07 in database ce07
+
+CREATE TABLE `planets` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `x_coord` int(11) unsigned NOT NULL,
+  `y_coord` int(11) unsigned NOT NULL,
+  `z_coord` int(11) unsigned NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `faction` varchar(20) NOT NULL DEFAULT 'Free Worlds',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
