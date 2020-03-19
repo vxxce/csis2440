eff2faa3edeab39c8fb9d291305d6f07466bd8c3
Zachary Olpin <info@zacharyolp.in>
2020-03-18 18:16:58 -0600

    fix regexp in prepared statements

--- public/pages/assignments/mysqlForm/scripts/submitSearch.php
+++ public/pages/assignments/mysqlForm/scripts/submitSearch.php
@@ -1,34 +1,47 @@
-<?php 
+<?php
 
 $errors = [
-    "invalidCharacters" => []
-  ];
+  "invalidCharacters" => [],
+  "missingData" => []
+];
 
-  // Check if any invalid characters
-  $errors["invalidCharacters"] = $anyInvalidChars($_POST, "/[^a-zA-Z0-9@#%~_\^\*\-\.\?\!\/]/");
+// Check if any invalid characters
+$errors["invalidCharacters"] = $anyInvalidChars($_POST, "/[^a-zA-Z0-9@#%~_\^\*\-\.\?\!\/]/");
 
-  // If any validation errors, compose header string with error info
-  if (array_sum(array_map("count", $errors))) {
-    $errorHeader = "Location: ../add.php?errors=true";
-    foreach ($errors as $errorType => $e) {
-      if (count($e)) {
-        $errorHeader .= "&$errorType=" . implode(",", $e);
-      }
+// Check if at least field has data
+if (empty($_POST["fname"]) && empty($_POST["lname"])) $errors["missingData"][] = "allFieldsEmpty";
+
+// If any validation errors, compose header string with error info
+if (array_sum(array_map("count", $errors))) {
+  $errorHeader = "Location: ../add.php?errors=true";
+  foreach ($errors as $errorType => $e) {
+    if ($e) {
+      $errorHeader .= "&$errorType=" . implode(",", $e);
     }
-    $_POST["errors"] = $errors;
-  } else {
-    try {
-      // If everything valid, prepare statement and attempt search
-      $query = $pdo->prepare("SELECT id, fname, lname from players WHERE fname=? OR lname=?");
+  }
+  $_POST["errors"] = $errors;
+} else {
+  try {
+    // If at least one field, make  queries conditional on which fields were completed
+    if ($_POST["fname"] && $_POST["lname"]) {
+      $query = $pdo->prepare("SELECT id, fname, lname from players WHERE fname REGEXP ? and lname REGEXP ? ORDER BY lname ASC, fname ASC");
       $success = $query->execute([
         strtolower($_POST["fname"]),
         strtolower($_POST["lname"])
       ]);
-      $res = $query->fetchAll(PDO::FETCH_ASSOC);
-      foreach ($res as $key => $value) {
-        print $value["id"] . ", " . $value["fname"] . ", " . $value["lname"] . "<br>";
-      }
-    } catch (\Exception $e) {
-      die;
+    } else if ($_POST["fname"]) {
+      $query = $pdo->prepare("SELECT id, fname, lname from players WHERE fname REGEXP ? ORDER BY fname ASC");
+      $success = $query->execute([strtolower($_POST["fname"])]);
+    } else {
+      $query = $pdo->prepare("SELECT id, fname, lname from players WHERE lname REGEXP ? ORDER BY lname ASC");
+      $success = $query->execute([strtolower($_POST["lname"])]);
+    };
+    // fetch results
+    $res = $query->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($res as $key => $value) {
+      print $value["id"] . ", " . $value["fname"] . ", " . $value["lname"] . "<br>";
     }
+  } catch (\Exception $e) {
+    print($e);
   }
+}
