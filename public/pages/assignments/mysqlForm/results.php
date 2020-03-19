2e02f945c0c5e1d52777364be826077b641fbf86
Zachary Olpin <info@zacharyolp.in>
2020-03-19 00:58:35 -0600

    display  results of query

--- public/pages/assignments/mysqlForm/results.php
+++ public/pages/assignments/mysqlForm/results.php
@@ -1,22 +1,47 @@
 <?php
+// Set default query type to add if no selection yet.
+$queryType = (!empty($_POST["queryType"])) ? $_POST["queryType"] : "add";
+require_once "scripts/utils.php";
+// Connect to db
+require_once $_SERVER["DOCUMENT_ROOT"] . "/../private/conn.php";
+require_once "scripts/utils.php";
+$pdo = new PDO($mysql_dsn, $mysql_un, $mysql_pw);
+?>
 
- // Connect to db
- require_once $_SERVER["DOCUMENT_ROOT"] . "/../private/conn.php";
- require_once "scripts/utils.php";
- $pdo = new PDO($mysql_dsn, $mysql_un, $mysql_pw);
+<!DOCTYPE html>
+<html lang="en">
 
-switch ($_POST["submit"]) {
-  case "ADD":
-    require_once "scripts/submitAdd.php";
-    break;
-  case "UPDATE":
-    require_once "scripts/submitUpdate.php";
-    break;
-  case "SEARCH":
-    require_once "scripts/submitSearch.php";
-    break;
-  default:
-    break;
-}
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Results</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
 
-if ($errorHeader) print $errorHeader;
+<body>
+  <main>
+    <form action="form.php" method="post">
+      <input type="submit" name="queryType" class="query-toggle <?= $active("add", $queryType) ?>" value="add">
+      <input type="submit" name="queryType" class="query-toggle <?= $active("update", $queryType) ?>" value="update">
+      <input type="submit" name="queryType" class="query-toggle <?= $active("search", $queryType) ?>" value="search">
+    </form>
+
+    <?php
+    switch ($_POST["submit"]) {
+      case "ADD":
+        require_once "scripts/submitAdd.php";
+        break;
+      case "UPDATE":
+        require_once "scripts/submitUpdate.php";
+        break;
+      case "SEARCH":
+        require_once "scripts/submitSearch.php";
+        break;
+      default:
+        break;
+    }
+    ?>
+  </main>
+</body>
+
+</html>
