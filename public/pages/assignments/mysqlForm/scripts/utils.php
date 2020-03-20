c00098bf3555109c6aedf80de541e30d7b033a8d
Zachary Olpin <info@zacharyolp.in>
2020-03-20 06:04:56 +0000

    refactor formatDate function to not rely on array_key_first (not available in php < 7.3)

--- public/pages/assignments/mysqlForm/scripts/utils.php
+++ public/pages/assignments/mysqlForm/scripts/utils.php
@@ -53,12 +53,13 @@ $formatDate = function (string $date) {
   if (!$formattedYMD[0]) {
     return;
   } else if ($formattedYMD[0] && $formattedYMD[2]) {
-    $formattedYMD[1] = str_pad($splitDate[array_key_first($splitDate)], 2, "0", STR_PAD_LEFT );
+    $formattedYMD[1] = str_pad($splitDate[array_keys($splitDate)[0]], 2, "0", STR_PAD_LEFT );
   } else {
-    $formattedYMD[1] = str_pad($splitDate[array_key_first($splitDate)], 2, "0", STR_PAD_LEFT );
-    $formattedYMD[2] = str_pad($splitDate[array_key_last($splitDate)], 2, "0", STR_PAD_LEFT );
+    $formattedYMD[1] = str_pad($splitDate[array_keys($splitDate)[0]], 2, "0", STR_PAD_LEFT );
+    $formattedYMD[2] = str_pad($splitDate[array_keys($splitDate)[1]], 2, "0", STR_PAD_LEFT );
   }
   ksort($formattedYMD);
+
   return implode("-", $formattedYMD);
 };
 
