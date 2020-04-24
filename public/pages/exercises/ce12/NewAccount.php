fe0a7f9fea4bd179f6ddff6927cfb7da8905576e
Zachary Olpin <info@zacharyolp.in>
2020-04-24 01:33:16 -0600

    server-side validation, barebones design

--- public/pages/exercises/ce12/NewAccount.php
+++ public/pages/exercises/ce12/NewAccount.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
-if (isset($_SESSION['email'])) header("Location: Dashboard.php");
+if (isset($_SESSION['loggedIn'])) header("Location: Dashboard.php");
 ?>
 
 <!DOCTYPE html>
-
 <html>
 
 <head>
@@ -14,58 +13,80 @@ if (isset($_SESSION['email'])) header("Location: Dashboard.php");
 </head>
 
 <body>
+
   <?php
-  if (isset($_POST['submit'])) {
-    // New account form submitted
+  if (!isset($_SESSION['errors'])) {
+    if (isset($_POST['submit'])) {
+      require_once "scripts/validateRegistration.php";
 
-    // TODO: PHP Validation
-    // TODO: Sanitize input
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $confirm = $_POST['confirm'];
+      $pass = $_POST['password'];
 
+      $errors = [];
+      if (!$isAlpha($name)) $errors[] = "Name may only include characters A-Z";
+      if (!$isValidLength($name, 0, 100)) $errors[] = "Name must be between 0 and 100 characters.";
+      if (!$isValidEmail($email)) $errors[] = "Email is invalid";
+      if ($pass !== $confirm) $errors[] = 'Passwords do not match.';
+      if (!$meetsPasswordRequirements($pass, "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/")) {
+        $errors[] = "Password must have 8+ characters including one numeric, one uppercase, and one special character.";
+      };
+      if (count($errors)) $_SESSION['errors'] = $errors;
 
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/../private/conn.php";
-    $pdo = new PDO($ce08_dsn, $ce08_un, $ce08_pw) or die("Could not connect");
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $pass = $_POST['confirm'];
-    $hash = password_hash($pass, PASSWORD_DEFAULT);
-    // Insert into DB
-    $query = $pdo->prepare(
-      "INSERT INTO ce08.captains (`name`, email, command, combat, commerce, cunning, pass)"
-        . "VALUES (?, ?, ?, ?, ?, ?, ?)"
-    );
-    $res = $query->execute([$name, $email, rand(25, 100), rand(25, 100), rand(25, 100), rand(25, 100), $hash]);
-    if ($res == true) {
-      $_SESSION["newUser"] = $email;
-      header("Location: Login.php");
+      require_once $_SERVER["DOCUMENT_ROOT"] . "/../private/conn.php";
+      $pdo = new PDO($ce08_dsn, $ce08_un, $ce08_pw) or die("Could not connect");
+      $hash = password_hash($pass, PASSWORD_DEFAULT);
+      // Insert into DB
+      $query = $pdo->prepare(
+        "INSERT INTO ce08.captains (`name`, email, command, combat, commerce, cunning, pass)"
+          . "VALUES (?, ?, ?, ?, ?, ?, ?)"
+      );
+      $res = $query->execute([$name, $email, rand(25, 100), rand(25, 100), rand(25, 100), rand(25, 100), $hash]);
+      if ($res == true) {
+        $_SESSION["newUser"] = $email;
+        header("Location: Login.php");
+      } else {
+        die("Something went wrong.");
+      }
     }
-  } else {
-    // No new account submission yet, show form.
-    print <<<HTML
-      <form action="NewAccount.php" method="POST" class="modal true">
-        <h2 class="form-header">Register</h1>
-        <div id="name-group" class="form-group">
-          <label for="name">Captain name: </label>
-          <input type="text" name="name" id="name" required maxlength="100">
-        </div>
-        <p class="error"></p>
-        <div id="email-group" class="form-group">
-          <label for="email">Email: </label>
-          <input type="email" name="email" id="email" required maxlength="300">
-        </div>
-        <p class="error"></p>
-        <div id="password-group" class="form-group">
-          <label for="password">Password: </label>
-          <input type="password" name="password" id="password" required>
-        </div>
-        <div id="confirm-group" class="form-group">
-          <label for="confirm">Confirm password: </label>
-          <input type="password" name="confirm" id="confirm" required>
-        </div>
-        <p class="error"></p>
-        <button class="submit" name="submit" id="submit" onclick="return isValid();">SUBMIT</button>
-      </form>
-      <script src="js/validateRegistration.js"></script>
-  </body>
-</html>
-HTML;
   }
+  ?>
+
+  <form action="NewAccount.php" method="POST" class="modal true">
+    <h2 class="form-header">Register</h2>
+    <ul>
+      <?php
+      if (isset($_SESSION['errors'])) {
+        foreach ($_SESSION['errors'] as $e) {
+          print "<li>$e</li>";
+        };
+      };
+      unset($_SESSION['errors']);
+      ?>
+    </ul>
+    <div id="name-group" class="form-group">
+      <label for="name">Captain name: </label>
+      <input type="text" name="name" id="name" required maxlength="100">
+    </div>
+    <p class="error"></p>
+    <div id="email-group" class="form-group">
+      <label for="email">Email: </label>
+      <input type="email" name="email" id="email" required maxlength="300">
+    </div>
+    <p class="error"></p>
+    <div id="password-group" class="form-group">
+      <label for="password">Password: </label>
+      <input type="password" name="password" id="password" required>
+    </div>
+    <div id="confirm-group" class="form-group">
+      <label for="confirm">Confirm password: </label>
+      <input type="password" name="confirm" id="confirm" required>
+    </div>
+    <p class="error"></p>
+    <button class="submit" name="submit" id="submit" onclick="return isValid();">SUBMIT</button>
+  </form>
+  <script src="scripts/validateRegistration.js"></script>
+</body>
+
+</html>
--- public/pages/exercises/ce12/authenticate.php
+++ public/pages/exercises/ce12/authenticate.php
@@ -1,29 +1,27 @@
 <?php
-
+// Db credentials
 require_once $_SERVER["DOCUMENT_ROOT"] . "/../private/conn.php";
+
+// Check if user email exists.
 $pdo = new PDO($ce08_dsn, $ce08_un, $ce08_pw) or die("Could not connect");
 $prep = $pdo->prepare("SELECT * FROM `ce08`.`captains` WHERE `email`=?");
 $prep->execute([$_POST["email"]]);
 $res = $prep->fetch(PDO::FETCH_ASSOC);
 $pdo = null;
 $prep = null;
+
 if (isset($res["email"])) {
+  // User exists
   if (password_verify($_POST["pass"], $res["pass"])) {
+    // And entered correcet password. Set Session variables.
     foreach ($res as $k => $v) $_SESSION[$k] = htmlentities($v);
+    $_SESSION['loggedIn'] = true;
+    unset($_SESSION['failedAttempts']);
     header("Location: Dashboard.php");
   } else {
     // User exists but password is incorrect
     $_SESSION["loginError"] = "Password was incorrect.";
-    if (isset($_SESSION['failedAttempts'])) {
-      $_SESSION['failedAttempts']++;
-      if ($_SESSION['failedAttempts'] > 3) { // or some number
-        // TODO: Time penalty
-      }
-    } else {
-      $_SESSION['failedAttempts'] = 0;
-    }
+    if (isset($_SESSION['failedAttempts'])) $_SESSION['failedAttempts']++; // TODO: Time penalty after 3 attempts
+    else $_SESSION['failedAttempts'] = 0;
   }
-} else {
-  // User does not exist
-  $_SESSION['loginError'] = "That user does not exist.";
-}
+} else $_SESSION['loginError'] = "That user does not exist.";
