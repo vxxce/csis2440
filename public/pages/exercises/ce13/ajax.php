67343f4cba5c394ebe4d6779d32331107f58d83c
Zachary Olpin <info@zacharyolp.in>
2020-05-10 12:14:50 -0600

    fix title

--- public/pages/exercises/ce13/ajax.php
+++ public/pages/exercises/ce13/ajax.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Distance Calculator</title>
+  <title>AJAX</title>
   <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
   <style>
     p {
