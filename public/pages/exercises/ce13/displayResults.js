568567d8ef306050ca45ed4457ccc8412490a69a
Zachary Olpin <info@zacharyolp.in>
2020-05-10 11:53:48 -0600

    render a billion divs

--- public/pages/exercises/ce13/displayResults.js
+++ public/pages/exercises/ce13/displayResults.js
@@ -1,7 +1,60 @@
-const displayResults = async () => {
-  const display = document.querySelector('#results')
-  const num = Number(document.querySelector('#num').value)
-  const url = `results.php?num=${num}`
-  await fetch(url).then(r => r.text()).then(r => display.innerHTML = r)
-  return false
-}
+// DOM element constants
+const fetchToggle = document.querySelector('#fetchToggle')
+const button = document.querySelector('button')
+const display = document.querySelector('#results')
+const num = document.querySelector('#num')
+
+
+  /////////////////////////////////////////////////////
+ //                                XMLHttpRequest   //
+//                                                 //
+const getEmXML = () => {                    //
+  const url = `results.php?num=${num.value}`     //
+  let req = new XMLHttpRequest()                //
+  req.onreadystatechange = () => {             //
+    if (req.readyState == 4) {                //
+      display.innerHTML = req.responseText   //
+    }                                       //
+  }                                        //
+  req.open("GET", url)                    //
+  req.send()                             // 
+}                                       //
+/////////////////////////////////////////
+
+
+  ////////////////////////////////////////////////////
+ //                                        FETCH   //
+//                                                //
+const getEmFetch = async () => {                 //
+  const url = `results.php?num=${num.value}`    //
+  req = await fetch(url)                       //
+  res = await req.text()                      //
+  display.innerHTML = res                    //
+}                                           //
+/////////////////////////////////////////////
+
+
+
+// attach listeners
+let fetching = false
+let url = ""
+button.addEventListener('click', getEmXML, false)
+fetchToggle.addEventListener('click', () => {
+  button.removeEventListener( 'click'
+                            , fetching
+                              ? getEmFetch
+                              : getEmXML
+                            , false
+  )
+  button.addEventListener( 'click'
+                           , fetching
+                              ? getEmXML
+                              : getEmFetch
+                          , false
+  )
+
+  fetching = !fetching
+  fetchToggle.checked = fetching
+  button.textContent = fetching ? "fetch!" : "XMLHttpRequest!"
+})
+num.addEventListener('input', e => url = e.target.value)
