9317c626092a47ebf45a63a1771c9c69ef8a5fa1
Zachary Olpin <info@zacharyolp.in>
2020-05-13 21:36:49 -0600

    taking out the trash

--- public/pages/exercises/exercises.php
+++ public/pages/exercises/exercises.php
@@ -15,20 +15,13 @@
     <h2>Exercises</h2>
     <div class='hr'></div>
     <div class="directory">
-      <div class="dirButton"><a href="/">Hello World</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce02/randomEvents.php">Random Events</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce03/FormPage.php">Using Forms</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce04/captainGenerator.php">Reading Files</a></div>
-      <div class="dirButton"><a href="/pages/exercises/ce05/screenshot.php">Workbench Setup</a></div>
-      <div class="dirButton"><a href="/pages/exercises/ce06/crud.php">Database CRUD</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce07/planetForm.php">Forms and Database</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce08/Login.php">Session & Login</a></div>
-      <div class="dirButton"><a href="/pages/exercises/ce09/index.html">JS Page</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce10/launchPanel.html">JS Control</a></div>
-      <div class="dirButton"><a href="/pages/exercises/ce11/shipSelection.html">JS Objects</a></div>
-      <div class="dirButton"><a href="/pages/exercises/ce12/Login.php">Validation</a></div>
       <div class="dirButton"><a href="/pages/exercises/ce13/ajax.php">AJAX</a></div>
-      <div class="dirButton"><a href="#">Shopping Cart</a></div>
     </div>
   </main>
   <script src="/assets/js/subDir.js" type="application/javascript"></script>
