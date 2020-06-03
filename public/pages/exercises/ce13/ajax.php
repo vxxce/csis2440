3c46cc445b2d32149b267de6fb4a2a94f9da3a6d
Zach <info@zacharyolp.in>
2020-06-03 04:27:10 -0600

    typo

--- public/pages/exercises/ce13/ajax.php
+++ public/pages/exercises/ce13/ajax.php
@@ -27,12 +27,12 @@
       How many divs do you want? I'll make you that many divs. I'll even color them, just for you. It will be hideous, I should warn you.
       I'm just going to color them randomly. But first, I'm just going to sit on your request for, oh, 10 seconds?
       I'll be doing basically nothing, just... making you wait. When you finally get them, you will have witnessed
-      something that is actually sort of amazing: Asynchronicity! You may not see what's amazing about it. If you want
+      something that is actually sort of amazing: Asynchrony! You may not see what's amazing about it. If you want
       to, though, I recommend watching <a href="https://www.youtube.com/watch?reload=9&v=8aGhZQkoFbQ">this!</a> And <a href="https://www.youtube.com/watch?v=8LCx9Dir8BU">this</a> too.
     </p>
     <p>
       Oh, and check out <a href="displayResults.js">the script</a> this page uses to see two different ways that you can make your request. Both will
-      demonstrate asynchronicity. You can make your request either way and the result will be the same. But you can toggle this toggler &rarr;<input type="radio" name="fetchToggle" id="fetchToggle">&larr; anyway to try one
+      demonstrate asynchrony. You can make your request either way and the result will be the same. But you can toggle this toggler &rarr;<input type="radio" name="fetchToggle" id="fetchToggle">&larr; anyway to try one
       or the other and I'll fix the button to let you know which one you're doing :) 
     </p>
     <hr>
